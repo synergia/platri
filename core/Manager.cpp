6c61d657bef60b6731fa9e95ecf96949273ff549
Tymon Tobolski <i@teamon.eu>
2010-08-23 15:33:04 +0200

    init

---
+++ core/Manager.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Manager.cpp
+ *  core
+ *
+ *  Created by Tymon Tobolski on 10-08-23.
+ *  Copyright 2010 Politechnika Wrocławska. All rights reserved.
+ *
+ */
+
+#include "Manager.h"
+
---
+++ core/Manager.h
@@ -0,0 +1,16 @@
+/*
+ *  Manager.h
+ *  core
+ *
+ *  Created by Tymon Tobolski on 10-08-23.
+ *  Copyright 2010 Politechnika Wrocławska. All rights reserved.
+ *
+ */
+
+#include "TuioListener.h"
+
+using namespace TUIO;
+
+class Manager : public TuioListener {
+	
+};
